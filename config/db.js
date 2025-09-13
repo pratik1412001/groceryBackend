eb12c5411bac4d5aadf7a217ecfd96cb0e07611d
Pratik Singh <141718144+pratik1412001@users.noreply.github.com>
2025-09-14 02:10:40 +0530

    new data

--- config/db.js
+++ config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
-    console.log("✅ MongoDB connected successfully    ffgfrff");
+    console.log("✅ MongoDB connected successfully");
   } catch (error) {
     console.error("❌ MongoDB connection error:", error);
     process.exit(1);
