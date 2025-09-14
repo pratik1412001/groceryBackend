aac2c289e51a742575e8b5053d2ced116cd1b04a
Pratik Singh <141718144+pratik1412001@users.noreply.github.com>
2025-09-15 01:00:50 +0530

    por

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const express  = require('express');
-const PORT = process.env.PORT || 7000;
+const PORT = process.env.PORT || 5000;
 const bodyParser = require("body-parser");
 const app = express();
 const connectDB = require('./config/db.js')
